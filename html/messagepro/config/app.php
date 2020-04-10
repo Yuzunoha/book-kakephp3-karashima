6f60d48f011dd36d9d79bd1d902b85d7fb845207
Yuzunoha <studysteadystandby@gmail.com>
2020-04-10 13:39:23 +0900

    db繋がった(ﾟ∀ﾟ)

--- html/messagepro/config/app.php
+++ html/messagepro/config/app.php
@@ -263,9 +263,9 @@ return [
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
-            'database' => 'my_app',
+            'username' => 'docker_db_user',
+            'password' => 'docker_db_user_pass',
+            'database' => 'docker_db',
             /*
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
              */
